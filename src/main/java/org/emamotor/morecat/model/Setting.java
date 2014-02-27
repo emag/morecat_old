f40b3052292867db1601eb0642936e2c56fe3565
emag <lanabe.lanabe@gmail.com>
2014-02-27 23:14:42 +0900

    Change table name

--- src/main/java/org/emamotor/morecat/model/Setting.java
+++ src/main/java/org/emamotor/morecat/model/Setting.java
@@ -1,11 +1,15 @@
 package org.emamotor.morecat.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 /**
  * @author tanabe
  */
 @Entity
+@Table(name = "settings")
+@Data
 public class Setting {
 
     @Id
