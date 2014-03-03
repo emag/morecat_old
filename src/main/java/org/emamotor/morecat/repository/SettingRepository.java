ef13ba978be43f75dac4b97ee8b2db3643e42296
emag <lanabe.lanabe@gmail.com>
2014-03-03 20:12:48 +0900

    Change into ejb

--- src/main/java/org/emamotor/morecat/repository/SettingRepository.java
+++ src/main/java/org/emamotor/morecat/repository/SettingRepository.java
@@ -2,9 +2,12 @@ package org.emamotor.morecat.repository;
 
 import org.emamotor.morecat.model.Setting;
 
+import javax.ejb.Stateless;
+
 /**
  * @author Yoshimasa Tanabe
  */
+@Stateless
 public class SettingRepository extends GenericRepository<Setting> {
 
     public SettingRepository() {
