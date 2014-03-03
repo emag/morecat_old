7e5c474182ef1e174c623f08e205a7bc0b909e0d
emag <lanabe.lanabe@gmail.com>
2014-03-03 12:50:58 +0900

    Change Setting property

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO settings (blog_name, blog_description, secret) VALUES ('default_name', 'default_description', true);
+INSERT INTO settings (blog_name, blog_description, publicity) VALUES ('default_name', 'default_description', false);
 INSERT INTO users (name, email) VALUES ('user1', 'user1@emamotor.org');
 INSERT INTO entries (title, content, author_id, created_at, modified_at) VALUES ('title1', 'content1', 1, now(), now());
 INSERT INTO tags (entry_id, value) VALUES (1, 'tag1');
