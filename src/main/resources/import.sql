6da64e78cbbae5c3c097eab51a036273f9b07125
emag <lanabe.lanabe@gmail.com>
2014-03-03 22:53:33 +0900

    Fix typo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO settings (blog_name, blog_description, publicity) VALUES ('default_name', 'default_description', false);
 INSERT INTO users (name, email) VALUES ('user1', 'user1@emamotor.org');
-INSERT INTO entries (title, content, author_id, created_at, last_modified_at, state, format) VALUES ('title1', 'content1', 1, now(), now(), 'public', 'markdown');
+INSERT INTO entries (title, content, author_id, created_at, last_modified_at, state, format) VALUES ('title1', 'content1', 1, now(), now(), 'PUBLIC', 'MARKDOWN');
 INSERT INTO tags (entry_id, value) VALUES (1, 'tag1');
