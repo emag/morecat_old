64a92e5adeec198d4a623a35b41646d56efa3514
emag <lanabe.lanabe@gmail.com>
2014-03-04 13:07:20 +0900

    Create entry edit view

---
+++ src/main/java/org/emamotor/morecat/admin/entry/EntryEditController.java
@@ -0,0 +1,35 @@
+package org.emamotor.morecat.admin.entry;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.emamotor.morecat.model.Entry;
+import org.emamotor.morecat.model.Setting;
+import org.emamotor.morecat.service.EntryService;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+@Model
+public class EntryEditController {
+
+    @Inject
+    private EntryService entryService;
+
+    @Getter
+    @Setter
+    private Entry entry = new Entry();
+
+    public void doFind() {
+        this.entry = entryService.findById(entry.getId());
+    }
+
+    public String   doSave() {
+        entryService.update(this.entry);
+        return "view?faces-redirect=true";
+    }
+
+}
