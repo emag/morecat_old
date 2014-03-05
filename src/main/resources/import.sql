229457115756be185353f08803ab7226f7cc0a13
emag <lanabe.lanabe@gmail.com>
2014-03-05 23:08:25 +0900

    Add test data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,4 @@ INSERT INTO settings (blog_name, blog_description, publicity) VALUES ('default_n
 INSERT INTO users (name, email) VALUES ('user1', 'user1@emamotor.org');
 INSERT INTO entries (title, content, author_id, created_at, state, format) VALUES ('title1', 'content1', 1, now(), 'PUBLIC', 'MARKDOWN');
 INSERT INTO tags (entry_id, value) VALUES (1, 'tag1');
+INSERT INTO tags (entry_id, value) VALUES (1, 'tag2');
