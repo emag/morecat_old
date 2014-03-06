f45a52fdd09506c09a643dddd5485ee07b56677c
emag <lanabe.lanabe@gmail.com>
2014-03-07 01:26:52 +0900

    Add converter class

---
+++ src/main/java/org/emamotor/morecat/admin/entry/TagConverter.java
@@ -0,0 +1,35 @@
+package org.emamotor.morecat.admin.entry;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+@FacesConverter("tagConverter")
+public class TagConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+
+        Set<String> tags = new HashSet<>();
+        for (String each : value.split(",")) {
+            tags.add(each.trim().replace(' ', '_'));
+        }
+        return tags;
+
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+
+        String bracketedTags = value.toString();
+        return bracketedTags.substring(1, bracketedTags.length() - 1);  // remove bracket
+
+    }
+
+}
