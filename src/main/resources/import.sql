0fe9a6f2b58f213cdb50f0df222494970de68123
emag <lanabe.lanabe@gmail.com>
2014-03-09 17:24:48 +0900

    Remove unnecessary state

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,7 +3,7 @@ INSERT INTO users (name, email) VALUES ('user1', 'user1@emamotor.org');
 
 INSERT INTO entries (title, content, permalink, author_id, created_at, state, format) VALUES ('title1', 'content1', 'permalink1', 1, now(), 'PUBLIC', 'MARKDOWN');
 
-INSERT INTO entries (title, content, permalink, author_id, created_at, state, format) VALUES ('title2', 'content2', 'permalink2', 1, now(), 'PRIVATE', 'HTML');
+INSERT INTO entries (title, content, permalink, author_id, created_at, state, format) VALUES ('title2', 'content2', 'permalink2', 1, now(), 'DRAFT', 'HTML');
 INSERT INTO tags (entry_id, value) VALUES (2, 'tag1');
 
 INSERT INTO entries (title, content, permalink, author_id, created_at, state, format) VALUES ('title3', 'content3', 'permalink3', 1, now(), 'DRAFT', 'MARKDOWN');
