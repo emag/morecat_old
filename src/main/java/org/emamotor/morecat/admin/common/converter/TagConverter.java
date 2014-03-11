c8640d029d955af42f24551dbd17b360422bd0e1
emag <lanabe.lanabe@gmail.com>
2014-03-12 00:30:50 +0900

    Change package

--- src/main/java/org/emamotor/morecat/admin/common/converter/TagConverter.java
+++ src/main/java/org/emamotor/morecat/admin/common/converter/TagConverter.java
@@ -1,4 +1,4 @@
-package org.emamotor.morecat.admin.entry;
+package org.emamotor.morecat.admin.common.converter;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
