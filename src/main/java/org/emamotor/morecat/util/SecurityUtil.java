de1502ccb576fff11b455261498fef55afd98922
emag <lanabe.lanabe@gmail.com>
2014-03-14 15:19:51 +0900

    Add SecurityUtil#validateReferer(fix)

--- src/main/java/org/emamotor/morecat/util/SecurityUtil.java
+++ src/main/java/org/emamotor/morecat/util/SecurityUtil.java
@@ -2,6 +2,9 @@ package org.emamotor.morecat.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * @author tanabe
  */
@@ -10,11 +13,30 @@ public class SecurityUtil {
     public static boolean validateReferer(String scheme,
                                           String domain,
                                           int port,
-                                          String contextPath,
-                                          String apiPath,
+                                          String path,
                                           String referer) {
 
         if (StringUtils.isBlank(referer)) return false;
+
+        try {
+
+            URI refererURI = new URI(referer);
+
+            System.out.println(refererURI.toString());
+            System.out.println(refererURI.getScheme());
+            System.out.println(refererURI.getHost());
+            System.out.println(refererURI.getPort());
+            System.out.println(refererURI.getPath() + "\n");
+
+            if (! refererURI.getScheme().equals(scheme)) return false;
+            if (! refererURI.getHost().equals(domain)) return false;
+            if (! (refererURI.getPort() == port)) return false;
+            if (! refererURI.getPath().startsWith(path)) return false;
+
+        } catch (URISyntaxException e) {
+            return false;
+        }
+
         return true;
 
     }
--- src/test/java/org/emamotor/morecat/util/SecurityUtilTest.java
+++ src/test/java/org/emamotor/morecat/util/SecurityUtilTest.java
@@ -13,25 +13,54 @@ public class SecurityUtilTest {
     private static final String SCHEMA = "http";
     private static final String DOMAIN = "localhost";
     private static final int PORT = 8080;
-    private static final String CONTEXT_PATH = "mc-admin";
-    private static final String API_PATH = "mc-admin";
+    private static final String PATH = "/morecat-api/mc-admin/";
 
     @Test
     public void should_return_true_for_valid_referer() throws Exception {
         // Verify
         assertThat(SecurityUtil.validateReferer(
-                SCHEMA, DOMAIN, PORT, CONTEXT_PATH, API_PATH,
+                SCHEMA, DOMAIN, PORT, PATH,
                 "http://localhost:8080/morecat-api/mc-admin/entries/view.xhtml"), is(true));
-
     }
 
     @Test
     public void should_return_false_for_invalid_referer() throws Exception {
         // Verify
         assertThat(SecurityUtil.validateReferer(
-                SCHEMA, DOMAIN, PORT, CONTEXT_PATH, API_PATH,
+                SCHEMA, DOMAIN, PORT, PATH,
                 "https://twitter.com/HiromitsuTakagi/status/350971098248118272"), is(false));
+    }
 
+    @Test
+    public void should_return_false_for_invalid_scheme_referer() throws Exception {
+        // Verify
+        assertThat(SecurityUtil.validateReferer(
+                SCHEMA, DOMAIN, PORT, PATH,
+                "https://localhost:8080/morecat-api/mc-admin/entries/view.xhtml"), is(false));
+    }
+
+    @Test
+    public void should_return_false_for_invalid_domain_referer() throws Exception {
+        // Verify
+        assertThat(SecurityUtil.validateReferer(
+                SCHEMA, DOMAIN, PORT, PATH,
+                "http://invalid.com:8080/morecat-api/mc-admin/entries/view.xhtml"), is(false));
+    }
+
+    @Test
+    public void should_return_false_for_invalid_port_referer() throws Exception {
+        // Verify
+        assertThat(SecurityUtil.validateReferer(
+                SCHEMA, DOMAIN, PORT, PATH,
+                "http://localhost:8000/morecat-api/mc-admin/entries/view.xhtml"), is(false));
+    }
+
+    @Test
+    public void should_return_false_for_invalid_path_referer() throws Exception {
+        // Verify
+        assertThat(SecurityUtil.validateReferer(
+                SCHEMA, DOMAIN, PORT, PATH,
+                "http://localhost:8080/morecat-api/invalid/entries/view.xhtml"), is(false));
     }
 
 }
