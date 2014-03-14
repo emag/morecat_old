a6c387634ed52a0220faff9c18d4864c4b90843d
emag <lanabe.lanabe@gmail.com>
2014-03-14 13:35:56 +0900

    Add LoginPageFilter

---
+++ src/main/java/org/emamotor/morecat/admin/common/filter/LoginPageFilter.java
@@ -0,0 +1,41 @@
+package org.emamotor.morecat.admin.common.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * To prevent user from going back to Login page if the user already logged in
+ * @author tanabe
+ */
+@WebFilter(urlPatterns = "/mc-admin/login.xhtml")
+public class LoginPageFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if(request.getUserPrincipal() != null){
+            response.sendRedirect(request.getContextPath()+ "/mc-admin/overview/view.xhtml");
+        } else{
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void destroy() {}
+
+}
