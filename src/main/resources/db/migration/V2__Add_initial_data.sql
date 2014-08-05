36ab3c6ca5409b809d44783e36e3667ee81f8826
emag <lanabe.lanabe@gmail.com>
2014-08-05 20:36:52 +0900

    Enable Host OS to connect DB

---
+++ src/main/resources/db/migration/V2__Add_initial_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO settings (version, blog_name, blog_description, publicity) VALUES (0, 'default_name', 'default_description', FALSE);
+
+INSERT INTO users (version, name, password, email, role) VALUES (0, 'admin','e028814727bf3bb50c902a453ba358a1864ff46dca56160b3c2f3f49ca2de8027bc867a6efcf6e01e7dc15c31cde8b637c254b0bacf6df8ea20f7081f356991e','admin@morec.at', 'ADMIN');
